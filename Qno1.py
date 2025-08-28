710e900e91c449e5e8a35ec8fe505a903db4181e
Bishal Raut <bishal.raut@axiossoftwork.com>
2025-08-28 20:17:47 +1000

    Issue:with the decryption need to work

--- Qno1.py
+++ Qno1.py
@@ -15,6 +15,8 @@ return chr((ord(ch) - ord('A') + shift) % 26 + ord('A')) this"""
 
 #defining the main function 
 def main():
+
+
     #the number of shift asked to the user 
     shift1=int(input("Enter the first shift number : "))
     shift2=int(input("Enter the second shift number : "))
@@ -22,6 +24,12 @@ def main():
     #getting the current working directory using the OS (built-in library)
     path=os.getcwd()
     print(path)
+
+
+
+    
+    #initilizing the variable encrypted_text
+    encrypted_text=""
 
    
 
@@ -33,25 +41,67 @@ def main():
         input_plain_text=plain_text.read()
 
 
-        print(input_plain_text)
-
-        #initilizing the variable encrypted_text
-        encrypted_text=""
 
         encrypted_text=encryption(input_plain_text,shift1,shift2)
-        print(encrypted_text)
 
-        #print(input_plain_text)
+        #closing the file
+        plain_text.close()
 
-    #text for encryption
-   #plain_text=input("Enter the plain text")
-    #print(plain_text.lower())
 
+
+       
+    with open("encrypted_txt.txt","w") as encrypted:
+        encrypted.write(encrypted_text)
+        encrypted.close()
+    
+
+
+    #testing for now Nedd to remove for the final version
+    print("the decrypted text is",decryption(encrypted_text,shift1,shift2))
+
+
+
+
+
+#function to decrypt the text
+def decryption(encrypted_text,shift1,shift2):
+    #inilized the decrypted_text
+    decrypted_text=""
+    #loop thorugh every chracter
+    for ch in encrypted_text:
+          #checking condition for the lower case
+          if ch.islower():
+              #condtion for the range a to m
+              if 'a' <= ch <= 'm':
+                shift = -(shift1 * shift2)
+                decrypted_text+= chr((ord(ch) - ord('a') + shift) % 26 + ord('a'))
+                #condtion for n to z
+              else:
+                shift = (shift1 + shift2)
+                decrypted_text += chr((ord(ch) - ord('a') + shift) % 26 + ord('a'))
+          elif ch.isupper():
+              #condtion for the A to M
+              if 'A' <= ch <= 'M':
+                shift = shift1
+                decrypted_text += chr((ord(ch) - ord('A') + shift) % 26 + ord('A'))
+              else:
+                #Condition for N to Z
+
+                shift = -(shift1 ** 2)
+                decrypted_text += chr((ord(ch) - ord('A') + shift) % 26 + ord('A'))
+    else:
+        #No change for the remaining
+        decrypted_text += ch
+  
+    #returning the decrypted text
+    return decrypted_text
 
 #funciton to handle the encryption 
 def encryption(plain_text,shift1,shift2):
     #setting the empty string to append 
     cipher_text=""
+
+
     #looping through each character
     for ch in plain_text:
         
